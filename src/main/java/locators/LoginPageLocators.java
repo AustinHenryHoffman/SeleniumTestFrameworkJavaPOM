b62e22f46f5b80540bcd693fe6307a949482effe
DrTautology <austinhenryhoffman@gmail.com>
2023-09-06 16:17:08 -0500

    Added new locators for user id.

--- src/main/java/locators/LoginPageLocators.java
+++ src/main/java/locators/LoginPageLocators.java
@@ -7,4 +7,7 @@ public class LoginPageLocators {
     public static final By EMAIL_INPUT = By.xpath("//input[@name='emailid']");
     public static final By PASSWORD_INPUT = By.id("password");
     public static final By SUBMIT_BUTTON = By.xpath("//input[@name='btnLogin']");
+    public static final By USER_ID_FIELD = By.xpath("/html/body/table/tbody/tr[4]/td[1][text()='User ID :']");
+    public static final By USER_ID = By.xpath("/html/body/table/tbody/tr[4]/td[2]");
+
 }
