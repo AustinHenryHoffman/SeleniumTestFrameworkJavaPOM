d6687d05995dbc6cabfec25b5398ca6d6d470341
DrTautology <austinhenryhoffman@gmail.com>
2023-09-06 16:18:26 -0500

    Updated comments. Added new element identification and assertions.

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,4 +1,5 @@
 package tests;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import locators.LoginPageLocators;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -9,37 +10,44 @@ import org.testng.annotations.Test;
 import pages.LoginPage;
 import testbase.BaseTest;
 import java.time.Duration;
+import org.testng.Assert;
 
 public class LoginTest extends BaseTest {
 
     @Test
     public void testLogin() {
         // The driver is inherited from the BaseTest class, so it's already initialized
-        // You can directly use it to navigate to a webpage
-        driver.get("https://demo.guru99.com/"); // Replace with your website URL
+        // Navigating to test page.
+        driver.get("https://demo.guru99.com/");
 
         // Create an instance of LoginPage and pass the driver to it
         LoginPage loginPage = new LoginPage(driver);
 
-        // Wait for the submit button to become visible (adjust the timeout as needed)
+        // Set up wait parameters
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Wait for up to 10 seconds
 
         // Perform login using Page Object methods
-        loginPage.enterEmail("abc@example.com"); // Replace with a valid email address
-        WebElement submitButton = wait.until(ExpectedConditions.visibilityOfElementLocated(LoginPageLocators.SUBMIT_BUTTON)); // Replace with the actual button locator
+        loginPage.enterEmail("abc@example.com");
+        //Verify that submit button is present
+        WebElement submitButton = wait.until(ExpectedConditions.visibilityOfElementLocated(LoginPageLocators.SUBMIT_BUTTON));
+        // Add a sleep of 5 seconds. I still don't understand why this is necessary,
+        // but the test will fail otherwise
+        try {
+            Thread.sleep(5000); // Sleep for 5 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         loginPage.clickSubmitButton();
-        // If there's a password field, enter it here: loginPage.enterPassword("your_password");
+        WebElement user_id_field = wait.until(ExpectedConditions.visibilityOfElementLocated(LoginPageLocators.USER_ID_FIELD));
+        String user_id_field_text = user_id_field.getText();
+        //System.out.println(user_id_field_text);
+        Assert.assertEquals(user_id_field_text, "User ID :");
+        WebElement user_id = driver.findElement(LoginPageLocators.USER_ID);
+        String user_id_text = user_id.getText();
+        //System.out.println("Element Text: " + user_id_text);
+        Assert.assertEquals(user_id_text, "mngr526051");
+        //wait.until(ExpectedConditions.jsReturnsValue("return document.readyState === 'complete';"));
 
-        // Verify that the submit button is visible
-        //if (submitButton.isDisplayed()) {
-            // Click the submit button
-            //submitButton.click();
-        //} else {
-            // Handle the case where the button is not visible
-            // You can throw an exception or take any necessary action
-
-       // }
-
-        // Add assertions or further actions as needed
     }
 }
+
