1c58eae535409812417cfdc1f8f74b8a9fb71160
DrTautology <austinhenryhoffman@gmail.com>
2023-09-08 15:04:39 -0500

    Tested the methods createResultsDirectory and saveScreenshot. Both appear to be working well. Still trying to resolve the issue with general logging for testcases.

--- src/main/java/utils/TestResultUtil.java
+++ src/main/java/utils/TestResultUtil.java
@@ -1,20 +1,37 @@
 package utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+
 import java.io.File;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TestResultUtil {
-
+    private static Logger logger = LogManager.getLogger(TestResultUtil.class);
     private WebDriver driver;
+    private String testRunName;
+    private String resultsDirName;
 
-    public TestResultUtil(WebDriver driver) {
+    public TestResultUtil(WebDriver driver, String testRunName) {
         this.driver = driver;
+        this.testRunName = testRunName;
+
+        // Create a unique directory for the test run
+        resultsDirName = createResultsDirectory();
+
+        // Configure Log4j to use this directory for log files
+        System.setProperty("logDirectory", resultsDirName);
+        //System.setProperty("logFilename", "test.log");
+        //Initialize logger after properties are set
+        // Initialize logger after properties are set
+        logger = LogManager.getLogger(TestResultUtil.class);
+
     }
 
     private String generateTimestamp() {
@@ -22,35 +39,34 @@ public class TestResultUtil {
         return dateFormat.format(new Date());
     }
 
-    private String createResultsDirectory() {
+    public String createResultsDirectory() {
         String timestamp = generateTimestamp();
-        String resultsDirName = "results/" + timestamp; // Adjust the path as needed
+        String resultsDirName = "results/" + testRunName + "_" + timestamp; // Unique directory for each test
         File resultsDir = new File(resultsDirName);
 
         if (!resultsDir.exists()) {
             if (resultsDir.mkdirs()) {
-                System.out.println("Results directory created: " + resultsDirName);
+                logger.info("Results directory created: " + resultsDirName);
             } else {
-                System.err.println("Failed to create results directory: " + resultsDirName);
+                logger.error("Failed to create results directory: " + resultsDirName);
             }
         }
 
         return resultsDirName;
     }
 
-    private void saveScreenshot(String resultsDir, String screenshotName) {
+    public void saveScreenshot(String screenshotName) {
         // Replace 'driver' with your WebDriver instance
+        String timestamp = generateTimestamp(); // Generate a timestamp
         File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        String screenshotPath = resultsDir + "/" + screenshotName + ".png";
+        String screenshotPath = resultsDirName + "/" + screenshotName + "_" + timestamp + ".png"; // Use logDirectory here
 
         try {
             Files.copy(screenshotFile.toPath(), new File(screenshotPath).toPath(), StandardCopyOption.REPLACE_EXISTING);
-            System.out.println("Screenshot saved: " + screenshotPath);
+            logger.info("Screenshot saved: " + screenshotPath);
         } catch (Exception e) {
-            System.err.println("Failed to save screenshot: " + e.getMessage());
+            logger.error("Failed to save screenshot: " + e.getMessage(), e);
         }
     }
-
-
 
 }
